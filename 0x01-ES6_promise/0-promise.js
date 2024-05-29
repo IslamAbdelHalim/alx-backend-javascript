b3bc5ed7a6d2ebb63574698a78da70da4dffa290
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-05-30 01:43:26 +0300

    a

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-  	const success = true;
-  	if (success) {
-  		resolve('success');
-  	} else {
-  		reject(new Error('failed'));
-  	}
+    const success = true;
+    if (success) {
+      resolve('success');
+    } else {
+      reject(new Error('failed'));
+    }
   });
 }
