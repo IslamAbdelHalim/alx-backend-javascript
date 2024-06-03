1dec732e4580f4bb38388433a2368def8941955a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-03 23:07:05 +0300

    a

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentIds(studentList) {
+  if (typeof studentList !== 'object') {
+    return [];
+  }
+
+  const listIds = studentList.map((ele) => ele.id);
+
+  return listIds;
+}
