2fe2a5e9604259dba17d9ee0195e7ae560c42b0d
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-04 23:17:13 +0300

    a

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,22 @@
 export default function cleanSet(set, startString) {
-  const start = startString;
-  const arr = [];
+  let str = '';
 
-  if (start.length === 0) {
+  if (startString === '') {
     return '';
   }
-
-  for (const ele of set) {
-    if (ele.startsWith(start)) {
-      arr.push(ele.slice(start.length));
+  for (const item of set) {
+    if (set instanceof Set && typeof startString === 'string') {
+      if (item && item.startsWith(startString)) {
+        const strSlice = item.slice(startString.length);
+        if (str !== '') {
+          /* Add hyphen only if it's not the first item, By adding the hyphen
+before the item (and only when str is not empty) */
+          str += '-';
+        }
+        str += strSlice;
+      }
     }
   }
 
-  return arr.join('-');
+  return str;
 }
