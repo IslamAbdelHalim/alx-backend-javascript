61a240093ce86cbb6d554d417a0d3318d93ae404
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-04 22:55:06 +0300

    buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,13 +2,12 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
 
   const typedArray = new Int8Array(buffer);
-  const dataView = new DataView(buffer)
 
   if (position >= typedArray.length) {
-    throw new Error('Position outside range');	
+    throw new Error('Position outside range');
   }
 
   typedArray[position] = value;
 
-  return typedArray;
+  return buffer;
 }
