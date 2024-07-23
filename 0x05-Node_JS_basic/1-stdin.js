9701fb5aa1c02c8947f75df0768eca2db5150c2f
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-23 19:29:47 +0300

    a

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name)
-    process.stdout.write('Your name is: ${name}\n');
+    process.stdout.write(`Your name is: ${name}`);
 });
 
-process.stding.on('end', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
