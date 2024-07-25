417bc7f746bfc52e14e5aa01413b9d93fa71c9e9
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-25 14:42:22 +0300

    modify error

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -3,15 +3,19 @@ function calculateNumber(type, a, b) {
     SUM: (a,b) => Math.round(a) + Math.round(b),
     SUBTRACT: (a,b) => Math.round(a) - Math.round(b),
     DIVIDE: (a,b) => {
-			try {
-				return Math.round(a) / Math.round(b);
-			} catch {
-				return 'Error'
-			}
+      if (Math.round(b) == 0)
+        return 'Error';
+      else
+      return Math.round(a) / Math.round(b)
     }
   }
 
   return operation[type](a, b);
 }
+
+console.log(calculateNumber('SUM', 1.4, 4.5))
+console.log(calculateNumber('SUBTRACT', 1.4, 4.5))
+console.log(calculateNumber('DIVIDE', 1.4, 4.5))
+console.log(calculateNumber('DIVIDE', 1.4, 0))
 
 module.exports = calculateNumber;
